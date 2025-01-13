dcf54b0db0406ed25b4818790c7d48e931db0d2f
HaseebAhmed-tech <hbmz4812@gmail.com>
2025-01-13 13:29:22 +0500

    ticker update

--- src/components/industry/ticker.js
+++ src/components/industry/ticker.js
@@ -16,7 +16,7 @@ const TickerComponent = () => {
             </div>
         </div>
     </Marquee>
-    <Marquee speed={50} gradient={false}  direction="right" style={{transform: "translate(0, -25%)"}}>
+    <Marquee speed={50} gradient={false}  direction="right" style={{transform: "translate(0, -35%)"}}>
         <div style={{width: "100vw", display: "flex", justifyContent: "space-around", }}>
             <div style={{display:'flex', alignItems:"center"}}>
                 <span className='marquee outline-text'>Spectreco</span>
